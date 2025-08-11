d237474bba8be5c7d6c8d75d1e533f3ffb8e802d
manfrim <rafaelmanfrim2004@gmail.com>
2025-08-11 00:44:31 -0300

    Teste redirecionamento para novo subdomínio

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -20,17 +20,16 @@ import { Cartoes } from '@/pages/Cartoes'
 export function Router() {
   return (
     <Routes>
-      {/* <Route path="/" element={<LandingPage />} /> */}
       <Route path="/login" element={<Login />} />
       <Route path="/registro" element={<Register />} />
       <Route path="/esqueci-minha-senha" element={<ForgotMyPassword />} />
       <Route path="/nova-senha/:token" element={<NewPassword />} />
       <Route path="/acesso-negado" element={<AcessoNegado />} />
-      <Route path="/calculadora" element={<AppLayout />}>
-        <Route path="/calculadora/" element={<App />} />
-        <Route path="/calculadora/cartoes" element={<Cartoes />} />
-        <Route path="/calculadora/historico" element={<History />} />
-        <Route path="/calculadora/perfil" element={<Profile />} />
+      <Route path="/" element={<AppLayout />}>
+        <Route path="/" element={<App />} />
+        <Route path="/cartoes" element={<Cartoes />} />
+        <Route path="/historico" element={<History />} />
+        <Route path="/perfil" element={<Profile />} />
       </Route>
       <Route path="/admin" element={<AdminLayout />}>
         <Route path="/admin" element={<Admin />} />
